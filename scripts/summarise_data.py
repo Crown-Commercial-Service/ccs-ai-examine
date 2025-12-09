97b00d81c943fbaba083d2312b51131871f97ed9
SamuelHLewis <sam.lewis1@crowncommercial.gov.uk>
2025-12-09 14:27:32 +0000

    Add line-level data output

--- scripts/summarise_data.py
+++ scripts/summarise_data.py
@@ -1,42 +1,42 @@
 import pandas as pd
 
 # read in data and make sure that column types are correct
-contracts = pd.read_csv("dummy_data/dummy_contracts.csv", low_memory=False)
-matched = pd.read_csv("dummy_data/dummy_combined.csv", low_memory=False)
-unmatched = pd.read_csv("dummy_data/dummy_unmatched_mi.csv", low_memory=False)
+contracts = pd.read_csv("data/contracts.csv", low_memory=False)
+matched = pd.read_csv("data/combined.csv", low_memory=False)
+unmatched = pd.read_csv("data/unmatched.csv", low_memory=False)
 # where a buyer-supplier pair has >1 contract, take the most recent contract
-reported_spend_per_contract = matched.sort_values('Contract Start Date', ascending=False).groupby('PairID').agg({
+reported_spend_per_pair = matched.sort_values('Contract Start Date', ascending=False).groupby('PairID').agg({
     'Contracting Authority': 'first',
     'Supplier': 'first',
-    'Minimum Contract Value': 'sum',
+    'Total Contract Value - High (GBP)': 'sum',
     'Contract Start Date': 'first',
     'Contract End Date': 'first',
     'Contract Duration (Months)': 'first',
     'EvidencedSpend': 'sum'
 }).reset_index(drop=True)
-reported_spend_per_contract['Contract Start Date'] = pd.to_datetime(reported_spend_per_contract['Contract Start Date'])
-reported_spend_per_contract['Contract End Date'] = pd.to_datetime(reported_spend_per_contract['Contract End Date'])
+reported_spend_per_pair['Contract Start Date'] = pd.to_datetime(reported_spend_per_pair['Contract Start Date'])
+reported_spend_per_pair['Contract End Date'] = pd.to_datetime(reported_spend_per_pair['Contract End Date'])
 # add a column of the number of months between each start date and the present day
 now = pd.Timestamp.now().normalize()
-reported_spend_per_contract['Total Months Run So Far'] = (
-    (now.year - reported_spend_per_contract['Contract Start Date'].dt.year) * 12 + 
-    (now.month - reported_spend_per_contract['Contract Start Date'].dt.month)
+reported_spend_per_pair['Total Months Run So Far'] = (
+    (now.year - reported_spend_per_pair['Contract Start Date'].dt.year) * 12 + 
+    (now.month - reported_spend_per_pair['Contract Start Date'].dt.month)
 )
 # find expired contracts
-reported_spend_per_contract['Expired'] = reported_spend_per_contract['Contract End Date'] < now
-expired_contracts = reported_spend_per_contract[reported_spend_per_contract['Expired']==True].copy()
+reported_spend_per_pair['Expired'] = reported_spend_per_pair['Contract End Date'] < now
+expired_contracts = reported_spend_per_pair[reported_spend_per_pair['Expired']==True].copy()
 
 # summary stats
 total_contracts = len(contracts)
-total_contracts_with_key = len(reported_spend_per_contract)
+total_contracts_with_key = len(reported_spend_per_pair)
 unmatched_mi_entries = len(unmatched)
 unique_unmatched_suppliers = len(unmatched['SupplierName'].unique())
 unique_unmatched_buyers = len(unmatched['CustomerName'].unique())
-total_contracts_with_spend = len(reported_spend_per_contract[reported_spend_per_contract['EvidencedSpend']>0.0])
+total_contracts_with_spend = len(reported_spend_per_pair[reported_spend_per_pair['EvidencedSpend']>0.0])
 red_filter = expired_contracts['EvidencedSpend']==0.0
 no_spend_expired = len(expired_contracts[red_filter])
-amber_filter = (reported_spend_per_contract['Expired']==False) & (reported_spend_per_contract['Total Months Run So Far']>3) & (reported_spend_per_contract['EvidencedSpend']==0.0)
-no_spend_3month_run = len(reported_spend_per_contract[amber_filter])
+amber_filter = (reported_spend_per_pair['Expired']==False) & (reported_spend_per_pair['Total Months Run So Far']>3) & (reported_spend_per_pair['EvidencedSpend']==0.0)
+no_spend_3month_run = len(reported_spend_per_pair[amber_filter])
 summary_stats = {
     "Summary Statistic": ["Total Contracts", "Total Contracts with Supplier Key", "Unmatched MI Entries", "Unique Unmatched Suppliers", "Unique Unmatched Buyers",
                           "Total Contracts with Spend", "Total Contracts No Spend and Expired", "Total Contracts No Spend and Running >3 Months"],
@@ -46,3 +46,6 @@ summary_stats = {
 summary_stats_df = pd.DataFrame(summary_stats)
 print(summary_stats_df)
 summary_stats_df.to_csv("data/summary_stats.csv", index=False)
+
+# line-level data output
+reported_spend_per_pair.to_csv("data/line_level.csv", index=False)
